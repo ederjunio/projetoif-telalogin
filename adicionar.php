2ea646cc60ad71fc4f30c07fcd51eadad3e2a525
Eder Junio Xavier <eder.xavier.ti@gmail.com>
2021-04-21 12:43:02 -0300

    aulas php if

---
+++ adicionar.php
@@ -0,0 +1,20 @@
+
+</<?php 
+    //Header
+    include_once 'includes/header.php';
+?>
+
+<div class="row">
+        <div class="col s12 m6 push-m3 ">
+            <h3 class="light">Novo Cliente</h3>
+            
+
+
+            
+        </div>
+</div>
+
+<?php
+    //Footer
+    include_once 'includes/footer.php';
+?>
--- includes/header.php
+++ includes/header.php
@@ -12,13 +12,3 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-
-    
-
-
-
-
-        <!--JavaScript at end of body for optimized loading-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    </body>
-  </html>
