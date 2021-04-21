0ab563a9d9a11e2e3c7ba512dcc38597de2b0df0
Eder Junio Xavier <eder.xavier.ti@gmail.com>
2021-04-21 12:42:31 -0300

    aulas php if

---
+++ index.php
@@ -0,0 +1,39 @@
+
+</<?php 
+    //Header
+    include_once 'includes/header.php';
+?>
+
+<div class="row">
+        <div class="col s12 m6 push-m3 ">
+            <h3 class="light">Clientes</h3>
+            <table class="striped">
+                <thead>
+                    <tr>
+                        <th>Nome: </th>
+                        <th>Sobrenome: </th>
+                        <th>E-mail: </th>
+                        <th>Idade: </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Eder</td>
+                        <td>Xavier</td>
+                        <td>eder.xavier.ti@gmail.com</td>
+                        <td>42</td>
+                        <td> <a href="" class="btn-floating orange"> <i class="material-icons">edit</i> </a></td>
+                        <td> <a href="" class="btn-floating red"> <i class="material-icons">delete</i> </a></td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <a href="" class="btn">Adicionar Cliente</a>
+        </div>
+</div>
+
+<?php
+    //Footer
+    include_once 'includes/footer.php';
+?>
